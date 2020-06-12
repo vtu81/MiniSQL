724ab903b37fdcd182025b917f92fea98cb04c54
vtu81 <48283858+vtu81@users.noreply.github.com>
2020-06-12 21:49:08 +0800

    在main模块中创建了3个manager的全局变量

---
+++ code/main.cpp
@@ -0,0 +1,17 @@
+#include "Catalog_Manager.h"
+#include "Index_Manager.h"
+#include "Record_Manager.h"
+#include "API.h"
+
+#include <iostream>
+
+API api;
+CatalogManager catalog_manager;
+RecordManager record_manager;
+IndexManager index_manager(&api);
+
+
+int main(int argc,char * argv[])
+{
+    
+}
