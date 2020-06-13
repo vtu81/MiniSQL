e93c309e67eb7178ba48e3f5c4b5b23232b16cf8
windsyu <58393981+windsyu@users.noreply.github.com>
2020-06-13 14:38:21 +0800

    Update CatalogManager.h

--- code/CatalogManager.h
+++ code/CatalogManager.h
@@ -1,84 +1,96 @@
 //
-//  catalog.hpp
-//  catalog
+//  
+//  catalog by 俞晓锋 
 //
 
 #ifndef _CATALOG_MANAGER_H_
-#define _CATALOG_MANAGER_H_ 1
+#define _CATALOG_MANAGER_H_
 
 #include <iostream>
 #include <cmath>
 #include <cstring>
 #include <iomanip>
-#include "buffer_manager.h"
-#include "basic.h"
-#include "exception.h"
-#include "const.h"
+#include "Buffer_Manager.h"
+#include "Basic.h"
+#include "Exception.h"
 
-extern BufferManager buffer_manager;
+
+
 
 class CatalogManager{
 public:
-    //输入：表名，属性对象，主键编号，索引对象
-    //输出：void
-    //功能：在catalog文件中插入一个表的元信息
-    //异常：如果已经有相同表名的表存在，则抛出table_exist异常
-    void createTable(std::string table_name, Attribute attribute, int primary, Index index);
-    //输入：表名
-    //输出：void
-    //功能：在catalog文件中删除一个表的元信息
-    //异常：如果表不存在，抛出table_not_exist异常
-    void dropTable(std::string table_name);
-    //输入：表名
-    //输出：bool
-    //功能：查找对应表是否存在，存在返回true，不存在返回false
-    //异常：无异常
-    bool hasTable(std::string table_name);
-    //输入：表名，属性名
-    //输出：bool
-    //功能：查找对应表中是否有某一属性，如果有返回true，如果没有返回false
-    //异常：如果表不存在，抛出table_not_exist异常
-    bool hasAttribute(std::string table_name , std::string attr_name);
-    //输入：表名
-    //输出：属性对象
-    //功能：获取一个表的属性
-    //异常：如果表不存在，抛出table_not_exist异常
-    Attribute getAttribute(std::string table_name);
-    //输入：表名，属性名，索引名
-    //输出：void
-    //功能：在catalog文件中更新对应表的索引信息（在指定属性上建立一个索引）
-    //异常：如果表不存在，抛出table_not_exist异常。如果对应属性不存在，抛出attribute_not_exist异常。
-    //如果对应属性已经有了索引，抛出index_exist异常。
-    void createIndex(std::string table_name,std::string attr_name,std::string index_name);
-    //输入：表名，索引名
-    //输出：索引所对应的属性名
-    //功能：通过索引名定位属性名
-    //异常：如果表不存在，抛出table_not_exist异常。如果对应索引不存在，抛出index_not_exist异常。
-    std::string IndextoAttr(std::string table_name,std::string index_name);
-    //输入：表名，索引名
-    //输出：void
-    //功能：删除对应表的对应属性上的索引
-    //异常：如果表不存在，抛出table_not_exist异常。如果对应属性不存在，抛出attribute_not_exist异常。
-    //如果对应属性没有索引，抛出index_not_exist异常。
-    void dropIndex(std::string table_name,std::string index_name);
-    //输入：表名
-    //输出：void
-    //功能：显示表的信息
-    //异常：如果表不存在，抛出table_not_exist异常
-    void showTable(std::string table_name);
+	BufferManager BM;
+	/*
+	@parammeter：table_name,attribute_name,primary_key,index
+	Function：insert a table in catalog file
+	Output：if repeat, output error table_not_exist
+	*/
+    void CreateTable(std::string t_name, Attribute attribute, int primary, Index index);
+	/*
+    @parammeter：table_name
+    Function：delete a table in catalog file
+    Output：if no such table, output error table_not_exist
+	*/
+    void DropTable(std::string t_name);
+	/*
+	@parammeter：table_name,attribute_name,index
+	Function：Create a index for an atrribute on a table
+	Output：if no such table，output error table_not_exist.
+	if no such attribute，output error:attribute_not_exist。
+	if index repeat，output error index_exist
+	*/
+	void CreateIndex(std::string t_name, std::string a_name, std::string i_name);
+	/*
+	@parammeter：table_name,index
+	Function：find attribute through index
+	Output：if no such table，output error table_not_exist
+			if no such index，output error index_not_exist
+	*/
+	std::string IndextoAttr(std::string t_name, std::string i_name);
+	/*
+	@parammeter：table_name,index
+	Function：Delete index of a table
+	output：if no such table，output error table_not_exist
+			if no such attribute, output error attribute_not_exist
+			if no such index，output error index_not_exist
+	*/
+	void DropIndex(std::string t_name, std::string i_name);
+    /*
+	@parammeter：table_name
+    Function：check if the table exists, ouput true for yes and false for no
+	*/
+    bool IsTable(std::string t_name);
+    /*
+	@parammeter：table_name, attribute_name
+    Function：check if the attribute exists, true for yes and false for no
+    Output：if no such attribute,output error table_not_exist
+	*/
+    bool IsAttribute(std::string t_name , std::string a_name);
+    /*
+	@parammeter：table_name
+    Function：Get the attribute from a named table
+    Ouput：if no such table，output error table_not_exist
+	*/
+    Attribute GetAttribute(std::string t_name);
+    /*
+	@parammeter：table_name
+    Function：show table information
+    output：if no such table，output error able_not_exist
+	*/
+    void PrintTable(std::string t_name);
 private:
-    //数字转字符串，bit为数的位数
-    std::string num2str(int num,short bit);
-    //字符串转数字
-    int str2num(std::string str);
-    //得到该行的表的名字
-    std::string getTableName(std::string buffer,int start,int &rear);
-    //返回表在文件中的位置,返回具体位置，引用传出数据所在的块信息
-    int getTablePlace(std::string name,int &suitable_block);
-    //返回该表的index
-    Index getIndex(std::string table_name);
-    //获取文件大小
-    int getBlockNum(std::string table_name);
+    /*tranform numer to string，bit is width of number*/
+    std::string numtostr(int num,short bit);
+    /*string to number*/
+    int strtonum(std::string str);
+    /*get the table name*/
+    std::string GetTableName(std::string buffer,int start,int &rear);
+    /*get the address of the table and return the block information */
+    int GetTableAdress(std::string t_name,int &suitable_block);
+    /*get the index of the table*/
+    Index GetIndex(std::string t_name);
+    /*get the size of the file*/
+    int GetBlockNum(std::string t_name);
 };
 
-#endif /* catalog_hpp */
+#endif
