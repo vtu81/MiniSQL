a12b168fea0ff710a4b2b2c2900fd06ee5104d7e
Ena-0731 <55282810+Ena-0731@users.noreply.github.com>
2020-06-14 17:25:06 +0800

    Update Condition.h

--- code/Condition.h
+++ code/Condition.h
@@ -9,6 +9,7 @@ class Condition
 {
 
 public:
+	//存储各种比较操作
 	const static int OPERATOR_EQUAL = 0; // "="
 	const static int OPERATOR_NOT_EQUAL = 1; // "<>"
 	const static int OPERATOR_LESS = 2; // "<"
@@ -16,12 +17,17 @@ public:
 	const static int OPERATOR_LESS_EQUAL = 4; // "<="
 	const static int OPERATOR_MORE_EQUAL = 5; // ">="
 
+	//构造函数
 	Condition(string a, string v, int o);
 
+	//待比较的attribute名称
 	string attributeName;
-	string value;           // the value to be compared
-	int operate;            // the type to be compared
+	//待比较的值
+	string value;
+	//待比较的attribute类型
+	int operate;
 
+	//对不同类型的content重载，进行比较
 	bool ifRight(int content);
 	bool ifRight(float content);
 	bool ifRight(string content);
