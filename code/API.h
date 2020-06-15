fa8f20c77613419bf4f3ab1ca516cdba7524b64a
vtu81 <48283858+vtu81@users.noreply.github.com>
2020-06-15 23:54:11 +0800

    API.h中若干接口变更！
    * Attribute现在是个结构，来自basic.h，有很大的变化！
    * dropIndex()添加一个参数 index_name

--- code/API.h
+++ code/API.h
@@ -6,10 +6,10 @@
 #include <vector>
 #include <string>
 #include "Condition.h"
-#include "Attribute.h"
+// #include "SingleAttribute.h" //原本是Attribute.h，现在的Attribute在Basic.h中
+#include "Basic.h"
 /*目录下暂时没有的头文件，以后可能会用到*/
 // #include "IndexInfo.h"
-// #include "Basic.h"
 // #include "Exception.h"
 
 using namespace std;
@@ -39,13 +39,13 @@ public:
     API(){};
     ~API(){};
     //创建表
-    void createTable(string table_name, vector<Attribute>* attributes, string primary_key_name, int primary_key_location);
+    void createTable(string table_name, Attribute attributes, string primary_key_name, int primary_key_location);
     //删除表
     void dropTable(string table_name);
     //创建索引
     void createIndex(string index_name, string table_name, string attribute_name);
     //删除索引
-    void dropIndex(string table_name);
+    void dropIndex(string table_name, string index_name);
     //展示记录 -- 无条件
     void showRecord(string table_name, vector<string>* attribute_names);
     //展示记录 -- 有条件
@@ -62,6 +62,8 @@ public:
     可能会被API本身、catalog manager、record manager、index manager模块调用
     没有列全，用到时自行补足即可*/
     void insertIndex(string index_name, char* key_addr, int type, int blockID);
+    void deleteRecordIndex(char* record_begin, int record_size, Attribute attributes, int blockID);
+    void insertRecordIndex(char* record_begin, int record_size, Attribute attributes, int blockID);
     //To be continued.
 };
 
