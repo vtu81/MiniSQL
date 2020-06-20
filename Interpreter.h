6b2c7cc245cceda4948542ded05f6ce3d51561cc
renH2 <64569759+renH2@users.noreply.github.com>
2020-06-20 11:39:56 +0800

    Add files via upload

---
+++ Interpreter.h
@@ -0,0 +1,30 @@
+#ifndef _INTERPRETER_H_
+#define _INTERPRETER_H_
+#include <string>
+#include <vector>
+#include <fstream>
+#include<algorithm>
+#include "basic.h"
+#include "API.h"
+#include "Exception.h"
+#include "Condition.h"
+
+using namespace std;
+class Interpreter
+{
+public:
+	API *api;
+	string filename;
+	//get the next word starting from index
+	//执行语句s
+	int interpreter(string s);
+	Interpreter() {};
+	~Interpreter() {};
+
+private:
+	string getWord(string s, int& index);
+	string getWord_no_symbol(string s, int& index);
+	//string转int，如果超出范围则返回0
+	int string2num(string s);
+};
+#endif;
