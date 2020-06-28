d4381e495a39b930a65412d090e6b92b60683611
renH2 <64569759+renH2@users.noreply.github.com>
2020-06-28 09:00:29 +0800

    Add files via upload

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,12 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include"Interpreter.h"
+#include "API.h"
 #include "Catalog_Manager.h"
 #include "Index_Manager.h"
 #include "Record_Manager.h"
-#include "API.h"
-
-#include <iostream>
+using namespace std;
 
 BufferManager buffer_manager;
 API api;
@@ -11,14 +14,68 @@ CatalogManager catalog_manager;
 RecordManager record_manager;
 IndexManager index_manager(&api);
 
-
-
-
-int main(int argc,char * argv[])
+int main()
 {
 
-    //因为构造3个manager的时候用到了api，因此在此处才初始化api的3个manager指针
-    api.cm = &catalog_manager;
-    api.rm = &record_manager;
-    api.im = &index_manager;
+	api.cm = &catalog_manager;
+	api.rm = &record_manager;
+	api.im = &index_manager;
+	record_manager.api = &api;
+	record_manager.bm = &buffer_manager;
+	record_manager.cm = &catalog_manager;
+
+	Interpreter s;
+	s.api = &api;
+	string query="",temp="";
+	/*query = "create table school (sid char(5), age int, primary key (sid) )";
+	s.interpreter(query);
+	query="create index index_sid on school (sid) ";
+	s.interpreter(query);
+	query = "drop table school ";
+	s.interpreter(query);
+	query = "drop index index_sid";
+	s.interpreter(query); */
+/*	ifstream in("test.txt");
+	while (!in.eof())
+	{
+		getline(in, temp);
+		query = query + temp;
+		int pos = query.find(";");
+		if (pos != query.npos)
+		{
+			query.erase(pos);
+			cout << query << endl;
+			s.interpreter(query);
+			query = "";
+		}
+	}*/
+	//执行SQL脚本文件
+	/*
+	query = "execfile test.txt";
+	s.interpreter(query);
+	*/
+	cout << "-------------MiniSQL interface-------------" << endl;
+	while (1)
+	{
+		getline(cin, temp);//可直接输入execfile test.txt;
+		query = query + temp;
+		int pos = query.find(";");
+		if (pos != query.npos)
+		{
+			query.erase(pos);
+			//返回1代表成功执行，0代表出现bug，2代表退出SQL
+			int condition=s.interpreter(query);
+			if (condition == 0)
+				cout << "Error!" << endl;
+			else if (condition == 2)
+			{
+				cout << "Exit program!" << endl;
+				break;
+			}
+			query = "";
+		}
+	}
+	int i;
+	cin >> i;
+	return 0;
 }
